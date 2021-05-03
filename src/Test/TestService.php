ea78ae09ca4b22614b8ea6e84039a8a224aa2be3
Jóhan H Dam-Davidsen <johan.davidsen@fjakkarin.com>
2021-05-03 15:03:58 -0200

    Added the TestService

---
+++ src/Test/TestService.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ *
+ */
+
+namespace CodeBureau\Pitu\Test;
+
+use CodeBureau\Pitu\Common\ClientHeader;
+use CodeBureau\Pitu\Common\Courier;
+use CodeBureau\Pitu\Common\Envelope;
+use CodeBureau\Pitu\Common\ServiceHeader;
+use CodeBureau\Pitu\Test\Messages\VehicleOwner;
+
+/**
+ * Class TestService
+ * @package CodeBureau\Pitu\Test
+ */
+class TestService
+{
+    /**
+     * This method calls the test service VehicleService and fetches the corresponding owner.
+     *
+     * @param $cpr
+     * @return VehicleOwner
+     */
+    public function getVehicleOwnerByCPR($cpr)
+    {
+        // Create envelope
+        $env = new Envelope();
+
+        $env->setUserId('');
+        $env->setId('?');
+        $env->setProtocolVersion('4.0');
+
+        // Add Client Header
+        $clientHeader = new ClientHeader();
+        $clientHeader
+            ->setInstance('PITU-TEST')
+            ->setMemberClass('GOV')
+            ->setMemberCode('Org1')
+            ->setSubsystemCode('v1');
+        $env->setClientHeader($clientHeader);
+
+        // Add Service Header
+        $serviceHeader = new ServiceHeader();
+        $serviceHeader
+            ->setInstance('PITU-TEST')
+            ->setMemberClass('GOV')
+            ->setMemberCode('Org1')
+            ->setSubsystemCode('vehicleRegister')
+            ->setServiceCode('getVehicleOwnerByPersonalcode')
+            ->setServiceVersion('v1');
+        $env->setServiceHeader($serviceHeader);
+
+        // Add Message
+        $vehicleOwner = new VehicleOwner();
+        $vehicleOwner
+            ->setPersonCode($cpr);
+        $env->setBody($vehicleOwner);
+
+        $message = $env->toXML();
+
+        // Send message
+        $courier = new Courier();
+        $response = $courier->sendMessage($message);
+
+        $person = new VehicleOwner();
+        $person
+            ->setPersonCode($cpr)
+            ->setFirstName($response['value'][1]['value'][0]['value'][0]['value'])
+            ->setLastName($response['value'][1]['value'][0]['value'][1]['value'])
+            ->setDateOfBirth($response['value'][1]['value'][0]['value'][2]['value']);
+
+        // Return answer
+        return $person;
+    }
+}
